cedab069fc695b24f3c9fd30aef90b07ab35bac6
Jogendra Majhi <jogendra.majhi@letsventure.com>
2025-05-04 16:23:11 +0530

    fixed linkedin flow

--- src/layouts/addSocialPages/socialPagesAPI.js
+++ src/layouts/addSocialPages/socialPagesAPI.js
@@ -9,14 +9,15 @@ async function fetchLinkedInProfile(code, redirectUri) {
     console.log('Payload:', paylod);
     const response = await AxiosManager.post('/api/v1/linkedin/exchange-token', paylod);
     console.log('Access Token:', response);
-    const accessToken =  response.data.accessToken;
-    if (accessToken) {
+    const data =  response.data;
+    if (data.status == "success") {
         // Store the access token in localStorage
-        localStorage.setItem("linkedin_access_token", accessToken);
+        localStorage.setItem("linkedin_access_token", data["accessToken"]);
 
     //     // Proceed with fetching the LinkedIn accounts
     //     // fetchAccountsFromAPI(accessToken);
     }
+    return data;
   } catch (error) {
     console.error('Failed to fetch LinkedIn access token:', error);
     throw error;
